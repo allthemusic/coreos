b6f853ce7aca9945d59144467a1b36e4990b7e1e
Ryan Taylor Long <ryan@rtlong.com>
2015-03-07 15:29:22 -0800

    Make launcher smarter about existing nodes and make it wait for IPFS

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'droplet_kit'
+gem 'oj'
 
 gem 'pry'
--- launch_some_servers.rb
+++ launch_some_servers.rb
@@ -29,7 +29,7 @@ def cluster_discovery_address
     URI.parse(@cluster_discovery_address)
   else
     @cluster_discovery_address = open('https://discovery.etcd.io/new').read
-    puts "Cluster Discovery Address = #{@cluster_discovery_address}"
+    warn "Cluster Discovery Address = #{@cluster_discovery_address}"
   end
   @cluster_discovery_address
 end
@@ -46,12 +46,35 @@ def node_data(nodeid)
   }
 end
 
+def wait_for_ipfs(ip_address)
+  count = 0
+  begin
+    open("http://#{ip_address}:5001/api/v0/id")
+  rescue => ex
+    # p count: count, ip_address: ip_address, ex: ex
+    count += 1
+    unless count >= 300
+      Thread.pass
+      sleep 1
+      STDERR.putc ?.
+      retry
+    end
+    raise
+  end
+end
+
 client = DropletKit::Client.new(access_token: ENV.fetch('DIGITALOCEAN_API_TOKEN'))
 
-nodes = NODE_COUNT.times.map { |nodeid|
+existing_nodes = client.droplets.all.to_a.select {|n| /core-\d+.allthemusic.org/.match(n.name) }
+
+nodes = (existing_nodes.length...(existing_nodes.length + NODE_COUNT)).map { |nodeid|
   droplet = DropletKit::Droplet.new(node_data(nodeid + 1))
   warn "Creating #{droplet.name}..."
-  client.droplets.create(droplet)
+  response = client.droplets.create(droplet)
+  unless response.is_a?(DropletKit::Droplet)
+    raise RuntimeError, response
+  end
+  response
 }
 
 ready_nodes, new_nodes = [], nodes
@@ -71,6 +94,47 @@ until new_nodes.empty?
   sleep 1
 end
 
+warn "\n\n"
+
+node_details = {}
 ready_nodes.each do |node|
-  puts format("%-30s %s", node.name, node.networks.v4.select{ |n| n.type == 'public' }.map(&:ip_address).join(' '))
+  ip_address = node.networks.v4.detect{ |n| n.type == 'public' }.ip_address
+  warn "Node #{node.name} is available at #{ip_address}"
+
+  node_details[node.name] = {
+    digital_ocean_id: node.id,
+    name: node.name,
+    ip_address: ip_address,
+    ssh_user: 'core',
+  }
+end
+
+warn "\n\n"
+
+warn "Running `ssh-keygen -R` for each node IP..."
+node_details.each_value do |node|
+  system "ssh-keygen", "-R", node[:ip_address]
+end
+
+warn "\nWaiting for IPFS to become available..."
+threads = []
+node_details.each_value do |n|
+  threads << Thread.new(n) do |node|
+    response = wait_for_ipfs(node[:ip_address])
+
+    warn "\n#{node[:name]} is ready\n"
+    node[:ipfs_peer_id] = Oj.load(response.read)['ID']
+
+    node_webui = "http://#{node[:ip_address]}:5001/webui"
+    system "open", node_webui
+  end
+end
+threads.each(&:join)
+
+File.open('last_batch.json', 'w') do |file|
+  data = {
+    discovery: cluster_discovery_address,
+    nodes: node_details,
+  }
+  file.puts Oj.dump(data, mode: :compat, indent: 2)
 end
